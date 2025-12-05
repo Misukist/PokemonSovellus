ae5d754d9b806a7f95b4c1f53994b99ff87cf24b
Misukisti <misukisti@gmail.com>
2025-12-05 12:17:23 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use("/api/auth" , authRoutes)
 // React build
 app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
 });
 
